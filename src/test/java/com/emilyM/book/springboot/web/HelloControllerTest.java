56f029030f0e77f523d981a36d8d161bfa9e0f92
Emily8777 <minniefox@naver.com>
2022-09-22 22:49:05 +0900

    new project 0922 - 1

--- src/test/java/com/emilyM/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/emilyM/book/springboot/web/HelloControllerTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class HelloControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
