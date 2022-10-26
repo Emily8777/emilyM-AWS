b1c617b7271bd5b2f740512e1fa696c5efc4af25
Emily8777 <minniefox@naver.com>
2022-10-26 23:19:59 +0900

    new project 1026 - 2(-p.140)

--- src/main/java/com/emilyM/book/springboot/web/IndexController.java
+++ src/main/java/com/emilyM/book/springboot/web/IndexController.java
@@ -2,7 +2,9 @@ package com.emilyM.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -10,4 +12,10 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
 }
